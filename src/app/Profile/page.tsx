5edc6055bd521a8c38131f85c3dd74c50a7a865c
ronyonka <rononyonka@gmail.com>
2025-03-21 15:05:54 +0300

    protected profile pages

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -1,9 +1,20 @@
-const Profile = () => {
-  return (
+import { getServerSession } from 'next-auth';
+import { options } from '../api/auth/[...nextauth]/options';
+import { redirect } from 'next/navigation';
+
+
+const Profile = async () => {
+    const session = await getServerSession(options);
+    if(!session){
+        redirect("/api/auth/signin?callbackUrl=/Profile");
+    }
+    return (
     <div>
         <h1>Profile Server Session</h1>
+        <p>{session?.user?.email}</p>
+        <p>{session?.user?.role}</p>
     </div>
-  )
+    )
 }
 
 export default Profile
