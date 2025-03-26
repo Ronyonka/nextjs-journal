e149e11a8511a247201f1abc803db931394ff529
ronyonka <rononyonka@gmail.com>
2025-03-26 22:50:12 +0300

    created header and added dark mode toggle

---
+++ src/components/Header.tsx
@@ -0,0 +1,47 @@
+import { shadow } from "@/app/styles/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./DarkModeToggle";
+
+function Header() {
+  const user = null;
+  return (
+    <header
+      className="bg-popover relative flex h-24 w-full items-center justify-between px-3 sm:px-8"
+      style={{ boxShadow: shadow }}
+    >
+      <Link className="flex items-end gap-2" href="/">
+        <Image
+          src="/journal.png"
+          height={60}
+          width={60}
+          alt="logo"
+          className="roundend-full"
+          priority
+        />
+        <h1 className="flex flex-col pb-1 text-2xl leading-6 font-semibold">
+          Free <span>Mind</span>
+        </h1>
+      </Link>
+
+      <div className="gap -4 flex">
+        {user ? (
+          "Logout"
+        ) : (
+          <>
+            <Button asChild className="hidden sm:block">
+              <Link href="/sign-up">Sign Up</Link>
+            </Button>
+            <Button asChild variant="outline">
+              <Link href="/login">Login</Link>
+            </Button>
+          </>
+        )}
+        <ModeToggle />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
