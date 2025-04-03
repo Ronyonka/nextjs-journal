ebb6b982296e9df76286000dbc8e7a2473483783
ronyonka <rononyonka@gmail.com>
2025-04-03 15:15:56 +0300

    minor changes

--- tests/unit/testSupabaseConnection.test.ts
+++ tests/unit/testSupabaseConnection.test.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL =
-  process.env.SUPABASE_URL || "https://hgeznfusgysfuvvzeduz.supabase.co";
-const SUPABASE_ANON_KEY =
-  process.env.SUPABASE_ANON_KEY ||
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhnZXpuZnVzZ3lzZnV2dnplZHV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMwNjQ0MjUsImV4cCI6MjA1ODY0MDQyNX0.u5Vpz5tdNpPZ1BXvlojutWEh2uVPWKKfN3ZslioQq_c";
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
